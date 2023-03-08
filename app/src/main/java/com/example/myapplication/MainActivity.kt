6c0fd5ac3f8a42086642e01d015f10062b984a46
habibullah339 <hakh9689@gmail.com>
2023-03-08 15:33:38 +0500

    Bottom Navigation With Fragments Added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.example.myapplication
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Toast.makeText(this, "habibi", Toast.LENGTH_SHORT).show()
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment= supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController=navHostFragment!!.findNavController()
+        val popupMenu= PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+
+                title=when(destination.id){
+                    R.id.categoryFragment -> "Categories"
+                    R.id.newsFragment -> "News"
+                    R.id.profileFragment-> "Profile"
+                    else -> "Live Streaming"
+                }
+            }
+
+
+        })
     }
 }
